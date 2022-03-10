1b23a39fe80dd99f5911f8706c68b8e9e7c739df
Raymond Kigai <raymondkigai@gmail.com>
2022-03-11 01:29:12 +0300

    C

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ *main - Entry point
+ *
+ *Return: Always 0 (success)
+ */
+int main(void)
+{
+      int a;
+      long int b;
+      long long int c;
+      char d;
+      float f;
+
+      printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(d));
+
